1084bef4171d96e4ad2bf2a5472c97d672c6f0c1
Vadim-BG <infotourbg.vadim@gmail.com>
2020-06-09 15:41:22 +0300

    Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы.

--- src/main/java/ru/nickoff/springcourse/ClassicalMusic.java
+++ src/main/java/ru/nickoff/springcourse/ClassicalMusic.java
@@ -2,6 +2,20 @@ package ru.nickoff.springcourse;
 
 public class ClassicalMusic implements Music {
 
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/ru/nickoff/springcourse/TestSpring.java
+++ src/main/java/ru/nickoff/springcourse/TestSpring.java
@@ -7,20 +7,25 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
+        ClassicalMusic classicalMusic= context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparison = firstmusicPlayer == secondmusicPlayer;
-        System.out.println(comparison);
-        System.out.println(firstmusicPlayer);
-        System.out.println(secondmusicPlayer);
-
-        firstmusicPlayer.setVolume(10);
-        System.out.println(firstmusicPlayer.getVolume());
-        System.out.println(secondmusicPlayer.getVolume());
+//        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstmusicPlayer == secondmusicPlayer;
+//        System.out.println(comparison);
+//        System.out.println(firstmusicPlayer);
+//        System.out.println(secondmusicPlayer);
+//
+//        firstmusicPlayer.setVolume(10);
+//        System.out.println(firstmusicPlayer.getVolume());
+//        System.out.println(secondmusicPlayer.getVolume());
 
         //musicPlayer.playMusicList();
 
