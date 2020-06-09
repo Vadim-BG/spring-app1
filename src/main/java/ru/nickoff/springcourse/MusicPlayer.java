e0d052636944380ec4a8fe22caa6ab52b208910e
Vadim-BG <infotourbg.vadim@gmail.com>
2020-06-09 14:48:40 +0300

    Внедрение зависимостей. Dependency Injection(DI). Часть 2. с использованием ArrayList

--- src/main/java/ru/nickoff/springcourse/MusicPlayer.java
+++ src/main/java/ru/nickoff/springcourse/MusicPlayer.java
@@ -1,7 +1,15 @@
 package ru.nickoff.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     private String name;
     private int volume;
@@ -22,19 +30,21 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+//    //IoC
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//
+//    public MusicPlayer() {
+//    }
+//
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
