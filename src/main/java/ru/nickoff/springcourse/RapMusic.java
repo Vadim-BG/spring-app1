8fe8a3b4e157c5f5c75c817f3713196668ee5f16
Vadim-BG <infotourbg.vadim@gmail.com>
2020-06-13 14:15:49 +0300

    Аннотации. Введение.

--- src/main/java/ru/nickoff/springcourse/RapMusic.java
+++ src/main/java/ru/nickoff/springcourse/RapMusic.java
@@ -2,6 +2,21 @@ package ru.nickoff.springcourse;
 
 public class RapMusic implements Music{
 
+    public RapMusic() {}
+
+    public static RapMusic getRapMusic(){
+        return new RapMusic();
+    }
+
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "U Can't Touch This";
--- src/main/java/ru/nickoff/springcourse/TestSpring.java
+++ src/main/java/ru/nickoff/springcourse/TestSpring.java
@@ -8,27 +8,18 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        ClassicalMusic classicalMusic= context.getBean("musicBean", ClassicalMusic.class);
 
-        System.out.println(classicalMusic.getSong());
+        Music music = context.getBean("rockMusic", Music.class);
 
-        //Music music = context.getBean("musicBean", Music.class);
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-//        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//
-//        boolean comparison = firstmusicPlayer == secondmusicPlayer;
-//        System.out.println(comparison);
-//        System.out.println(firstmusicPlayer);
-//        System.out.println(secondmusicPlayer);
-//
-//        firstmusicPlayer.setVolume(10);
-//        System.out.println(firstmusicPlayer.getVolume());
-//        System.out.println(secondmusicPlayer.getVolume());
+        musicPlayer.playMusic();
 
-        //musicPlayer.playMusicList();
+        Music music2 = context.getBean("classicalMusic", Music.class);
 
+        MusicPlayer classicMusicalPlayer = new MusicPlayer(music2);
+
+        classicMusicalPlayer.playMusic();
 
         context.close();
     }
